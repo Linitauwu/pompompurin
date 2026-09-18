aa3c8fe35ee93576ed0e823d274cfd37c3e9356f
Linitauwu <xombixc@gmail.com>
2026-09-18 16:58:30 -0600

    Conectar inicio de sesión con Menu y Cocina

--- Proyecto Pompompurin/RobotMesero/src/components/navigation/AppNavigator.tsx
+++ Proyecto Pompompurin/RobotMesero/src/components/navigation/AppNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import LoginScreen from '../screens/LoginScreen';
 import MenuScreen from '../screens/MenuScreen';
 import CategoriaScreen from '../screens/CategoriaScreen';
 import CocinaScreen from '../screens/CocinaScreen';
@@ -12,25 +13,15 @@ function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Menu"
+        initialRouteName="Login"
         screenOptions={{
           headerShown: false,
         }}
       >
-        <Stack.Screen
-  name="Menu"
-  component={MenuScreen}
-/>
-
-<Stack.Screen
-  name="Categoria"
-  component={CategoriaScreen}
-/>
-
-<Stack.Screen
-  name="Cocina"
-  component={CocinaScreen}
-/>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Menu" component={MenuScreen} />
+        <Stack.Screen name="Categoria" component={CategoriaScreen} />
+        <Stack.Screen name="Cocina" component={CocinaScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
